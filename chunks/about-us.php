37a57194bdd39678444278341c5e44c7996baf2e
Ajaybhaskar-Athi <ajaybhaskar.athi@gmail.com>
2024-11-19 03:34:10 +0530

    Amaravati Foods

---
+++ chunks/about-us.php
@@ -0,0 +1,8 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">This project is proudly developed by <b> Athi Ajay Bhaskar</b> and <b> Vishnum Naidu </b>, passionate B.Tech students specializing in Full Stack Development at <b>  Lovely Professional University, Punjab </b>. Currently in their 3rd year of B.Tech in Computer Science and Engineering, they bring their skills and dedication to create seamless and efficient digital solutions.</p>
+				  
+			</div>
+	</section>
---
+++ chunks/description.php
@@ -0,0 +1,17 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Restaurant Powered By Anurag Projects</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+		    <p>Welcome to <b> Nakka Hari Hara Tirumalarao's </b> Online Store, a new initiative by  <b> Anurag Projects</b>, a renowned organization specializing in hotels and contracting businesses. Headed by Mr. Nakka Hari Hari Tirumalarao, a visionary entrepreneur from Sileru, Andhra Pradesh, Anurag Projects is now expanding its reach into the online retail space to serve the community better.</p>
+			
+			<p>This platform marks the beginning of Anurag Projects' journey into the digital marketplace, offering a simple and convenient way for customers to browse and book items online. It reflects Mr. Tirumalarao’s dedication to innovation and his commitment to enhancing everyday convenience for the people of Sileru and beyond.</p>
+			<p>At Nakka Hari Hari Tirumalarao's Online Store, we aim to provide an enjoyable and seamless shopping experience, continuing the excellence and reliability that Anurag Projects is known for.</p>
+			<p>Thank you for being a part of this exciting new chapter. We look forward to serving you!</p>
+			</div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
